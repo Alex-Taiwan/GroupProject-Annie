d9c3bc5eb76d0f001e52b44e489b04b1f385fd8c
Alex-Taiwan <59194576+Alex-Taiwan@users.noreply.github.com>
2020-01-07 14:52:56 +0800

    1-7
    剩下手機板跟swiper

---
+++ 團體專案-安妮公主花園/1-7-HTML (前端原檔)/template.js
@@ -0,0 +1,154 @@
+$(window).on("scroll", function () {
+    var windowScrollTop = $(window).scrollTop()
+    if (windowScrollTop > 100) {
+        $(".returnBut").fadeIn()
+    }
+    else {
+        $(".returnBut").fadeOut()
+    }
+})
+
+$(".returnBut").on("click", function () {
+
+
+
+    $('html,body').animate({ scrollTop: 0 },1000,"linear");   /* 返回到最頂上 */
+
+    return false;
+})
+
+// $(window).on("resize",function () {
+//     if($(window).width() > 850){
+//         $(".nav_btn_guide").hide()
+//         $(".mobile_nav_btns").hide()
+
+//             if($(".mobile_nav_btns").hasClass("active")){
+//                 $(".mobile_nav_btns").removeClass("active")
+//             }
+
+//             if (burger_decoration1.hasClass("active")) {
+//                 $(".burger_decoration1").stop()
+//                 $(".burger_decoration2").stop()
+//                 $(".burger_decoration3").stop()
+//                 $(".burger_decoration1").removeClass("active")
+//                 $(".burger_decoration2").removeClass("active")
+//                 $(".burger_decoration3").removeClass("active")
+//             }
+//     }
+
+// })
+
+$(function () {
+    $(window).on("resize", function () {
+        var wdth = $(window).width();
+        if (wdth > 850) {
+            $(".nav_btn_guide").hide()
+            $(".mobile_nav_btns").removeClass("active")
+            $(".mobile_nav_btn").removeClass("active")
+            $(".mobile_mask").hide()
+            $(".burger_decoration1").removeClass("active")
+            $(".burger_decoration2").removeClass("active")
+            $(".burger_decoration3").removeClass("active")
+
+            // if ($(".mobile_nav_btns").hasClass("active")) {
+            //     $(".mobile_nav_btns").removeClass("active")
+            // }
+
+            // if (burger_decoration1.hasClass("active")) {
+            //     // $(".burger_decoration1").stop()
+            //     // $(".burger_decoration2").stop()
+            //     // $(".burger_decoration3").stop()
+            //     $(".burger_decoration1").removeClass("active")
+            //     $(".burger_decoration2").removeClass("active")
+            //     $(".burger_decoration3").removeClass("active")
+            // }
+        }
+        if (wdth <= 850) {
+            $(".nav_btn_guide").show()
+            // $(".nav_btn_guide i").on("click", function () {
+            //     if ($(".guide_extraPages").hasClass("active")) {
+            //         $(".guide_extraPages").removeClass("active")
+            //     }
+            //     else {
+            //         $(".guide_extraPages").addClass("active")
+            //     }
+        
+            // })
+        
+        }
+    })
+
+})
+
+
+   
+    $(".nav_btn_guide i").on("click", function () {
+        if ($(".guide_extraPages").hasClass("active")) {
+            $(".guide_extraPages").removeClass("active")
+            // $(".nav_btn_eat").removeClass("specialActive")
+            // $(".nav_btn_wedding ").removeClass("specialActive")
+            // $(".nav_btn_talk").removeClass("specialActive")
+            // $(".nav_btn_homestay").removeClass("specialActive")
+
+        }
+        else {
+            $(".guide_extraPages").addClass("active")
+            // $(".nav_btn_eat").addClass("specialActive")
+            // $(".nav_btn_wedding ").addClass("specialActive")
+            // $(".nav_btn_talk").addClass("specialActive")
+            // $(".nav_btn_homestay").addClass("specialActive")
+        }
+
+    })
+
+
+
+
+$(".nav_btnGroup_mobile ").on("click", function () {
+    $(".mobile_mask").toggle()
+    // $(".mobile_nav_btns").toggle()
+    $(".guide_extraPages").removeClass("active")
+    var burger_decoration1 = $(".burger_decoration1")
+
+    if ($(".mobile_nav_btns").hasClass("active")) {
+        $(".mobile_nav_btns").removeClass("active")
+    }
+    else {
+        $(".mobile_nav_btns").addClass("active")
+    }
+
+    if ($(".mobile_nav_btn").hasClass("active")) {
+        $(".mobile_nav_btn").removeClass("active")
+    }
+    else {
+        $(".mobile_nav_btn").addClass("active")
+    }
+
+    if (burger_decoration1.hasClass("active")) {
+        $(".burger_decoration1").removeClass("active")
+        $(".burger_decoration2").removeClass("active")
+        $(".burger_decoration3").removeClass("active")
+        
+    }
+    else {
+        $(".burger_decoration1").addClass("active")
+        $(".burger_decoration2").addClass("active")
+        $(".burger_decoration3").addClass("active")
+        
+    }
+
+})
+
+$(".mobile_mask").on("click",function(){
+    $(".mobile_mask").hide()
+    // $(".mobile_nav_btn").css({
+    //     "width":"0vw",
+    // })
+    $(".mobile_nav_btn").removeClass("active")
+    $(".mobile_nav_btns").removeClass("active")
+    $(".guide_extraPages").removeClass("active")
+    $(".burger_decoration1").removeClass("active")
+    $(".burger_decoration2").removeClass("active")
+    $(".burger_decoration3").removeClass("active")
+    
+})
